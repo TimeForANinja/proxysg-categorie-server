a9999d898b9ec6edafe77258d625369481994f87
Tobias Kutscha <t.kutscha@yahoo.de>
2024-07-01 00:14:30 +0200

    frontend: build test-table

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,87 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Select from 'react-select';
+
+interface IURL {
+    id: number;
+    hostname: string;
+    memberOf: number[];
+}
+
+interface ICategory {
+    value: number;
+    label: string;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [urls, setURLs] = React.useState<IURL[]>([]);
+    const [categories, setCategory] = React.useState<ICategory[]>([]);
+    const [expandedId, setExpandedId] = React.useState<number | null>(null);
+
+    const handleExpand = (id: number) => {
+        setExpandedId(prevId => prevId === id ? null : id);
+    }
+
+    React.useEffect(() => {
+        const getURLs = async () => {
+            const response = await fetch('/api/urls');
+            const data = await response.json();
+            return data;
+        }
+
+        const getCategories = async () => {
+            const response = await fetch('/api/categories');
+            const data = await response.json();
+            return data;
+        }
+
+        Promise.all([getURLs(), getCategories()])
+            .then(([urlsData, categoriesData]) => {
+                setURLs(urlsData);
+                setCategory(categoriesData);
+            })
+            .catch((error) => console.error("Error:", error));
+    }, []);
+
+    return (
+        <div className="App">
+            <div className="App-body">
+                <table className="table" style={{width: '100%'}}>
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Hostname</th>
+                        <th>Categories</th>
+                        <th>Details</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {urls.map(url => (
+                        <React.Fragment key={url.id}>
+                            <tr key={url.id}>
+                                <td>{url.id}</td>
+                                <td>{url.hostname}</td>
+                                <td>
+                                    <Select
+                                        defaultValue={categories.filter(category => url.memberOf.includes(category.value))}
+                                        isMulti
+                                        options={categories}
+                                    />
+                                </td>
+                                <td>
+                                    <button onClick={() => handleExpand(url.id)}>{"<"}</button>
+                                </td>
+                            </tr>
+                            <tr key={url.id + '-expand'} style={{display: expandedId === url.id ? '' : 'none'}}>
+                                <td colSpan={99}>Hello there</td>
+                            </tr>
+                        </React.Fragment>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
 }
 
 export default App;
