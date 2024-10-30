64ca8d31b242cd3d4c8c7c00ee22bc9942d2a032
Tobias Kutscha <t.kutscha@yahoo.de>
2024-10-30 07:00:25 +0100

    frontend: bugfixes removing react-select

--- frontend/src/pages/api-tokens.tsx
+++ frontend/src/pages/api-tokens.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {getAPITokens, IApiToken} from "../api/tokens";
 import {getCategories, ICategory} from "../api/categories";
-import Select from "react-select";
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -14,16 +13,26 @@ import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import CheckIcon from '@mui/icons-material/Check';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import {colors} from "../api/colormixer";
+import MenuItem from "@mui/material/MenuItem";
 
 function BuildRow(props: { token: IApiToken, categories: ICategory[]}) {
     const { token, categories } = props;
     const [isCopied, setIsCopied] = React.useState(false);
     const [hideToken, setHideToken] = React.useState(false);
+    const [isCategories, setCategories] = React.useState(categories.filter(category => token.categories.includes(category.id)).map(c => c.id));
 
     const handleCopy = async () => {
         await navigator.clipboard.writeText(token.token);
         setIsCopied(true);
         setTimeout(() => setIsCopied(false), 1500);  // Change back after 3 seconds
+    };
+
+    const handleChange = (event: SelectChangeEvent<number[]>) => {
+        if (Array.isArray(event.target.value)) {
+            setCategories(event.target.value);
+        }
     };
 
     return (
@@ -44,10 +53,23 @@ function BuildRow(props: { token: IApiToken, categories: ICategory[]}) {
                 </TableCell>
                 <TableCell align="right">
                     <Select
-                        defaultValue={categories.filter(category => token.categories.includes(category.id)).map(c => ({ value: c.id, label: c.name }))}
-                        isMulti
-                        options={categories.map(c => ({ value: c.id, label: c.name }))}
-                    />
+                        multiple
+                        value={isCategories}
+                        label="Categories"
+                        onChange={handleChange}
+                        displayEmpty
+                        renderValue={(selected) => selected.map(c => categories.find(cat => cat.id === c)?.name).join(', ')}
+                    >
+                        {
+                            categories.map((cat, catIDX) => {
+                                return (
+                                    <MenuItem key={catIDX} value={cat.id}>
+                                        <div style={{ background: cat.color}}>{cat.name}</div>
+                                    </MenuItem>
+                                )
+                            })
+                        }
+                    </Select>
                 </TableCell>
             </TableRow>
     )
