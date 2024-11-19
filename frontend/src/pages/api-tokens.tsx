3b21f99e8a1243509fd0fdd0390a4f229f7a55a8
Tobias Kutscha <t.kutscha@yahoo.de>
2024-11-20 00:49:55 +0100

    frontend: add edit/create dialog for apitoken/categories

--- frontend/src/pages/api-tokens.tsx
+++ frontend/src/pages/api-tokens.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+
+import React, { useState } from 'react';
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Button, TextField} from '@mui/material';
 import {getAPITokens, IApiToken} from "../api/tokens";
 import {getCategories, ICategory} from "../api/categories";
 import Table from '@mui/material/Table';
@@ -17,6 +19,7 @@ import DeleteIcon from '@mui/icons-material/Delete'
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import {colors} from "../api/colormixer";
 import MenuItem from "@mui/material/MenuItem";
+import Box from "@mui/material/Box";
 
 function BuildRow(props: { token: IApiToken, categories: ICategory[]}) {
     const { token, categories } = props;
@@ -81,6 +84,8 @@ function BuildRow(props: { token: IApiToken, categories: ICategory[]}) {
 function ApiTokenPage() {
     const [tokens, setTokens] = React.useState<IApiToken[]>([]);
     const [categories, setCategory] = React.useState<ICategory[]>([]);
+    const [selectedToken, setSelectedToken] = React.useState<IApiToken | null>(null);
+    const [isDialogOpen, setDialogOpen] = React.useState(false);
 
     React.useEffect(() => {
         Promise.all([ getCategories(), getAPITokens()])
@@ -91,26 +96,102 @@ function ApiTokenPage() {
             .catch((error) => console.error("Error:", error));
     }, []);
 
+    const handleEditOpen = (token: IApiToken) => {
+        setSelectedToken(token);
+        setDialogOpen(true);
+    };
+
+    const handleDialogClose = () => {
+        setDialogOpen(false);
+    };
+
+    const handleSave = (token: IApiToken) => {
+        if (token.id === -1) {
+            // add new token
+            token.id = Math.max(...tokens.map(t => t.id)) + 1;
+            setTokens([...tokens, token]);
+        } else {
+            // "replace" existing token if id matches
+            setTokens(tokens.map(t => t.id === token.id ? token : t));
+        }
+        setDialogOpen(false);
+    };
+
     return (
-        <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>ID</TableCell>
-                        <TableCell>Description</TableCell>
-                        <TableCell align="right">Token</TableCell>
-                        <TableCell>Last Used</TableCell>
-                        <TableCell align="right">Categories</TableCell>
-                        <TableCell></TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {tokens.map(token =>
-                        <BuildRow key={token.id} token={token} categories={categories}></BuildRow>
-                    )}
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>ID</TableCell>
+                            <TableCell>Description</TableCell>
+                            <TableCell align="right">Token</TableCell>
+                            <TableCell>Last Used</TableCell>
+                            <TableCell align="right">Categories</TableCell>
+                            <TableCell></TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {tokens.map(token =>
+                            <BuildRow key={token.id} token={token} categories={categories}></BuildRow>
+                        )}
+                        <TableRow>
+                            <TableCell colSpan={5} align="center">
+                                <Button onClick={() => handleEditOpen({ id: -1, token: '', description: '', categories: [], lastUse: 'never' })}>
+                                    + Add Token
+                                </Button>
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
+                {selectedToken && (
+                <EditDialog
+                    isOpen={isDialogOpen}
+                    token={selectedToken}
+                    onClose={handleDialogClose}
+                    onSave={handleSave}
+                />
+            )}
+        </>
+    );
+}
+
+function EditDialog(props: {
+    isOpen: boolean,
+    token: IApiToken,
+    onClose: () => void,
+    onSave: (category: IApiToken) => void
+}) {
+    const { isOpen, token, onClose, onSave } = props;
+    const [description, setDescription] = React.useState(token.description);
+    const [tokenStr, setTokenStr] = React.useState(token.token);
+
+    React.useEffect(() => {
+        if (token) {
+            setDescription(token.description);
+            setTokenStr(token.token);
+        }
+    }, [token]);
+
+    const handleSave = () => {
+        onSave({ ...token, description, token: tokenStr });
+    };
+
+    return (
+        <Dialog open={isOpen} onClose={onClose}>
+            <DialogTitle>Edit Category</DialogTitle>
+            <DialogContent>
+                <Box display="flex" flexDirection="column" gap={2}>
+                    <TextField label="Description" value={description} onChange={(e) => setDescription(e.target.value)} />
+                    <TextField label="Token" value={tokenStr} onChange={(e) => setTokenStr(e.target.value)} />
+                </Box>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleSave}>Save</Button>
+                <Button onClick={onClose}>Cancel</Button>
+            </DialogActions>
+        </Dialog>
     );
 }
 
--- frontend/src/pages/categories.tsx
+++ frontend/src/pages/categories.tsx
@@ -11,14 +11,37 @@ import TableRow from '@mui/material/TableRow';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit"
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
-function BuildRow(props: { category: ICategory }) {
+const ModalStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+
+function BuildRow(props: {
+    category: ICategory,
+    onEdit: (category: ICategory) => void,
+}) {
     const { category } = props;
-    const [isColor, setColor] = React.useState(category.color);
-
-    const handleChange = (event: SelectChangeEvent) => {
-        setColor(Number(event.target.value));
-        // setAge(event.target.value as string);
+    const handleEdit = (category: ICategory) => {
+        props.onEdit(category);
     };
 
     return (
@@ -26,30 +49,21 @@ function BuildRow(props: { category: ICategory }) {
             <TableCell>{category.id}</TableCell>
             <TableCell>{category.name}</TableCell>
             <TableCell>
-                <Select
-                    value={isColor.toString()}
-                    label="Color"
-                    onChange={handleChange}
-                >
-                    {
-                        Array.from(Object.values(colors)).map((color, colorIDX) => {
-                            return (
-                                <MenuItem value={colorIDX}>
-                                    <div style={{backgroundColor: color}}>{color}</div>
-                                </MenuItem>
-                            )
-                        })
-                    }
-                </Select>
+                <div style={{ backgroundColor: colors[category.color], width: '20px', height: '20px' }} />
             </TableCell>
             <TableCell>{category.description}</TableCell>
-            <TableCell><DeleteIcon/></TableCell>
+            <TableCell>
+                <EditIcon onClick={() => handleEdit(category)} />
+                <DeleteIcon />
+            </TableCell>
         </TableRow>
-    )
+    );
 }
 
 function CategoriesPage() {
     const [categories, setCategory] = React.useState<ICategory[]>([]);
+    const [selectedCategory, setSelectedCategory] = React.useState<ICategory | null>(null);
+    const [isDialogOpen, setDialogOpen] = React.useState(false);
 
     React.useEffect(() => {
         Promise.all([getCategories()])
@@ -58,24 +72,115 @@ function CategoriesPage() {
             })
             .catch((error) => console.error("Error:", error));
     }, []);
+    const handleEditOpen = (category: ICategory) => {
+        setSelectedCategory(category);
+        setDialogOpen(true);
+    };
+
+    const handleDialogClose = () => {
+        setDialogOpen(false);
+    };
+
+    const handleSave = (category: ICategory) => {
+        if (category.id === -1) {
+            // add new category
+            category.id = Math.max(...categories.map(cat => cat.id)) + 1;
+            setCategory([...categories, category]);
+        } else {
+            // "replace" existing category if id matches
+            setCategory(categories.map(cat => cat.id === category.id ? category : cat));
+        }
+        setDialogOpen(false);
+    };
 
     return (
+        <>
         <TableContainer component={Paper}>
             <Table>
                 <TableHead>
-                <TableRow>
-                    <TableCell component="th" scope="row">ID</TableCell>
-                    <TableCell>Name</TableCell>
-                    <TableCell>Color</TableCell>
-                    <TableCell>Description</TableCell>
-                    <TableCell></TableCell>
-                </TableRow>
+                    <TableRow>
+                        <TableCell component="th" scope="row">ID</TableCell>
+                        <TableCell>Name</TableCell>
+                        <TableCell>Color</TableCell>
+                        <TableCell>Description</TableCell>
+                        <TableCell></TableCell>
+                    </TableRow>
                 </TableHead>
                 <TableBody>
-                {categories.map(cat => <BuildRow category={cat}/>)}
+                    {categories.map(cat => <BuildRow category={cat} onEdit={handleEditOpen} />)}
+                    <TableRow>
+                        <TableCell colSpan={5} align="center">
+                            <Button onClick={() => handleEditOpen({ id: -1, name: '', description: '', color: 0 })}>
+                                + Add Category
+                            </Button>
+                        </TableCell>
+                    </TableRow>
                 </TableBody>
             </Table>
         </TableContainer>
+        {selectedCategory && (
+            <EditDialog
+                isOpen={isDialogOpen}
+                category={selectedCategory}
+                onClose={handleDialogClose}
+                onSave={handleSave}
+            />
+        )}
+        </>
+    );
+}
+
+function EditDialog(props: {
+    isOpen: boolean,
+    category: ICategory,
+    onClose: () => void,
+    onSave: (category: ICategory) => void
+}) {
+    const { isOpen, category, onClose, onSave } = props;
+    const [name, setName] = React.useState(category.name);
+    const [description, setDescription] = React.useState(category.description);
+    const [color, setColor] = React.useState(category.color);
+
+    React.useEffect(() => {
+        if (category) {
+            setName(category.name);
+            setDescription(category.description);
+            setColor(category.color);
+        }
+    }, [category]);
+
+    const handleSave = () => {
+        // TODO: when changing color it's for some reason shifted by one
+        onSave({ ...category, name, description, color });
+    };
+
+    return (
+        <Dialog open={isOpen} onClose={onClose}>
+            <DialogTitle>Edit Category</DialogTitle>
+            <DialogContent>
+                <Box display="flex" flexDirection="column" gap={2}>
+                    <TextField label="Name" value={name} onChange={(e) => setName(e.target.value)} />
+                    <TextField label="Description" value={description} onChange={(e) => setDescription(e.target.value)} />
+                    <Select
+                        value={color.toString()}
+                        label="Color"
+                        onChange={(e) => setColor(Number(e.target.value))}
+                    >
+                        {
+                            Array.from(Object.values(colors)).map((color, colorIDX) => (
+                                <MenuItem key={colorIDX} value={colorIDX}>
+                                    <div style={{ backgroundColor: color, width: '20px', height: '20px' }} />
+                                </MenuItem>
+                            ))
+                        }
+                    </Select>
+                </Box>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleSave}>Save</Button>
+                <Button onClick={onClose}>Cancel</Button>
+            </DialogActions>
+        </Dialog>
     );
 }
 
