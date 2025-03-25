329b9af5baf264624e205a07a9c3c9f370dafcc3
TimeForANinja <t.kutscha@yahoo.de>
2025-03-25 12:23:00 +0100

    backend: add code to save webpage from /dist folder

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,8 @@
+import os
+from os.path import abspath
+
 from apiflask import APIFlask
+from flask import send_from_directory
 
 from db import db_singleton
 from routes.auth import auth_bp
@@ -8,7 +12,14 @@ from routes.token import token_bp
 from routes.url import url_bp
 
 # Initialize APIFlask instead of Flask
-app = APIFlask(__name__, "ProxySG Category Server", version="1.0.0", docs_path="/docs")
+app = APIFlask(
+    __name__,
+    "ProxySG Category Server",
+    version="1.0.0",
+    docs_path="/docs",
+#    static_url_path="/",
+    static_folder="./dist",
+)
 
 # Register blueprints
 app.register_blueprint(category_bp)
@@ -16,6 +27,25 @@ app.register_blueprint(history_bp)
 app.register_blueprint(auth_bp)
 app.register_blueprint(token_bp)
 app.register_blueprint(url_bp)
+
+
+# Serve index.html for the root route
+@app.get('/', defaults={'path': ''})
+@app.get('/<path:path>')
+def catch_all(path: str):
+    """Catch-all route for non-API routes."""
+    static_folder = abspath('./dist/')
+    static_file = os.path.join(static_folder, path)
+
+    print("catch_all", static_file)
+
+    # Check if the requested static file exists
+    if os.path.isfile(static_file):
+        return send_from_directory(static_folder, path)
+
+    # Fallback to serving index.html
+    return send_from_directory(static_folder, 'index.html')
+    #return app.send_static_file('index.html')
 
 
 # add "status" and "status_code" fields to the default flask errors
