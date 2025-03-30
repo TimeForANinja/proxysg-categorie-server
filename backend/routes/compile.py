9e220fe486201f7e38f1e6986388ca5aa78704a0
Tobias Kutscha <t.kutscha@yahoo.de>
2025-03-30 17:52:36 +0200

    backend: add nested categories support to compile

--- backend/routes/compile.py
+++ backend/routes/compile.py
@@ -1,9 +1,52 @@
 from datetime import datetime
 
 from apiflask import APIBlueprint
+
+from db.category import Category
 from db.db_singleton import get_db
 
 compile_bp = APIBlueprint('compile', __name__)
+
+def find_subcategories(
+        current_cat: Category,
+        categories_dict: dict[int, Category],
+        visited: set[int],
+        result: list[Category]
+):
+    for nested_id in current_cat.nested_categories:
+        # resolve category id to category in lookup table
+        nested_cat = categories_dict.get(nested_id)
+
+        # make sure we could lookup the nested category
+        if not nested_cat:
+            continue
+
+        # make sure we did not yet visit the nested category
+        # this should prevent infinite loops with circular nested categories
+        if nested_cat.id in visited:
+            continue
+
+        # store cat in visited and result list
+        visited.add(current_cat.id)
+        result.append(current_cat)
+
+        # call recursively for the nested cat
+        find_subcategories(nested_cat, categories_dict, visited, result)
+
+def calc_sub_cats(cat: Category, categories: list[Category]) -> list[Category]:
+    # Create a mapping of category IDs to category objects for quick lookup
+    categories_dict = {category.id: category for category in categories}
+
+    # List to store the subcategories
+    result = []
+    # Set to track already visited categories
+    visited = set()
+
+    # Start recursive traversal
+    find_subcategories(cat, categories_dict, visited, result)
+
+    return result
+
 
 @compile_bp.get("/api/compile/<string:token_uuid>")
 @compile_bp.doc(summary="Compile Categories", description="Compile Categories for the provided Token")
@@ -33,15 +76,21 @@ def handle_compile(token_uuid: str):
     response += f'; Generated on {datetime.now()}\n\n'
 
     for cat in token_cats:
+        # calculate subcategories (excluding the "root" cat itself)
+        sub_cats = calc_sub_cats(cat, categories)
+
         # header for the category
         response += f'; Category: {cat.name}\n'
         response += f'; Description: {cat.description}\n'
+        response += f'; Sub-Categories: {', '.join([c.name for c in sub_cats])}\n'
         response += f'define category "{cat.name}"\n'
 
         # fill in URLs that are part of the category
         for url in urls:
             if cat.id in url.categories:
                 response += f'  {url.hostname}\n'
+            elif any(sub_cat.id in url.categories for sub_cat in sub_cats):
+                response += f'  {url.hostname} ; from sub-cat {cat.name}\n'
 
         # end of category
         response += f'  ; end of {cat.name}\n'
