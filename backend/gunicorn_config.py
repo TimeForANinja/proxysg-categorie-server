8e1fa0e7c069dbb866130765335814dd269e4ff7
TimeForANinja <t.kutscha@yahoo.de>
2025-04-22 16:50:45 +0200

    backend: potential bugfix for on_starting

--- backend/gunicorn_config.py
+++ backend/gunicorn_config.py
@@ -1,6 +1,9 @@
+from typing import Any
 from app import initialize_app, app
 
-def on_starting():
+def on_starting(_server: Any):
+    # the server parameter must be defined, or else gunicorn crashes
+
     # Call initialize_app to trigger background tasks scheduler
     # this will only get called for the master worker
     initialize_app(app)
